3509728fdb082f372840297a517ee89081c8d9b2
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-03-17 20:42:19 -0400

    change states to be a function containing day, days array, and appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,78 +1,58 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
-import Appointment from 'components/Appointment/index';
-
-
+import Appointment from "components/Appointment/index";
+import getAppointmentsForDay from "helpers/selectors"
 
 export default function Application(props) {
 
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState(prev => ({ ...prev, day }));
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  const setAppointments = appointments => setState(prev => ({ ...prev, appointments}));
 
   useEffect(() => {
-  axios
-  .get('http://localhost:8001/api/days')
-  .then((res) => setDays(res.data))
-  .catch((err) => console.log(`There was an error when trying to get days ${err}`))
-  }, [days])
-
-  const appointments = [
-    {
-      id: 1,
-      time: "12pm",
-    },
-    {
-      id: 2,
-      time: "1pm",
-      interview: {
-        student: "Lydia Miller-Jones",
-        interviewer: {
-          id: 1,
-          name: "Sylvia Palmer",
-          avatar: "https://i.imgur.com/LpaY82x.png",
-        }
-      }
-    },
-    {
-      id: 3,
-      time: "3pm",
-      interview: {
-        student: "Ibreim",
-        interviewer: {
-          id: 2,
-          name: "Sylvester Pam",
-          avatar: "https://i.imgur.com/LpaY82x.png",
-        }
-      }
-    }, 
-    {
-      id: "last",
-      time: "8pm"
-    }
-  ];
-
-
-  
-const [ day, setDay ] = useState("Monday");
+    // Get request to get days
+    // Axios
+    // .get("http://localhost:3001/api/days")
+    // .then((res) => setDays(res.data))
+    // .catch((err) => console.log("There was an error when trying to get days", err))
+    // // Get request to get appointments
+    // Axios
+    // .get("http://localhost:3001/api/appointments")
+    // .then((res) => setAppointments(res.data))
+    // .catch((err) => console.log("There was an error when trying to get appointments", err))
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments")
+    ])
+    .then((res) => {
+      setDays(res[0].data)
+      setAppointments(res[1].data)
+    })
+  }, [])
 
   return (
     <main className="layout">
       <section className="sidebar">
+        {/* Replace this with the sidebar elements during the "Environment Setup" activity. */}
         <img
           className="sidebar--centered"
           src="images/logo.png"
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-
         <DayList
-          days={days}
-          day={day}
+          days={state.days}
+          day={state.day}
           setDay={setDay}
         />
-
         <nav className="sidebar__menu" />
         <img
           className="sidebar__lhl sidebar--centered"
@@ -81,9 +61,11 @@ const [ day, setDay ] = useState("Monday");
         />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => 
-          <Appointment key={appointment.id} {...appointment} /> )}
-        <Appointment key="last" time="5pm" />
+        {getAppointmentsForDay(state, state.day).map((appointment) => 
+          <Appointment 
+            key={appointment.id} 
+            {...appointment}/>
+        )}
       </section>
     </main>
   );
---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export default function getAppointmentsForDay(state, day) {
+  let aptArr = [];
+  for (let date of state.days) {
+    if (date.name === day) {
+      aptArr.push(...date.appointments);
+    }
+  }
+
+  let appointmentsArr = [];
+  for (let aptObj in state.appointments) {
+    for (let id of aptArr) {
+      if (id === parseInt(aptObj)) {
+        appointmentsArr.push(state.appointments[aptObj])
+      }
+    }
+  }
+  return appointmentsArr;
+}
