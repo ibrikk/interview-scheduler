d7f5f37fba7d23077345f9069768f3ab83728b51
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-03-18 17:16:28 -0400

    build hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,22 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return {mode};
+  const transition = (change, replace) => {
+    if (!replace) {
+    setHistory(history => ([...history, change]));
+    }
+    return setMode(change);
+  }
+
+const back = () => {
+  history.pop();
+  return setMode(history[history.length - 1]);
+}
+
+  return {mode, transition, back};
 }
 
 export default useVisualMode;
